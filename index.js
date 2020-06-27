0780e4c50d36ee12bdffd615bc4829bffbcfb851
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-06-26 23:45:53 -0500

    Import/Export js files/ content

--- index.js
+++ index.js
@@ -768,22 +768,24 @@
 
 //? Use getters and setters to Control Access to an Object
 
-class Thermostat {
-  constructor (fahrenheit) {
-    this.fahrenheit = fahrenheit
-  }
-  get temperature () {
-    return (5 / 9) * (this.fahrenheit - 32)
-  }
-  set temperature (celcius) {
-    this.fahrenheit = (celcius * 9.0) / 5 + 32
-  }
-}
+// class Thermostat {
+//   constructor (fahrenheit) {
+//     this.fahrenheit = fahrenheit
+//   }
+//   get temperature () {
+//     return (5 / 9) * (this.fahrenheit - 32)
+//   }
+//   set temperature (celcius) {
+//     this.fahrenheit = (celcius * 9.0) / 5 + 32
+//   }
+// }
 
-const thermos = new Thermostat(76)
-let temp = thermos.temperature
-thermos.temperature = 26
-temp = thermos.temperature
+// const thermos = new Thermostat(76)
+// let temp = thermos.temperature
+// thermos.temperature = 26
+// temp = thermos.temperature
 
-console.log(temp)
-console.log(thermos.temperature)
+// console.log(temp)
+// console.log(thermos.temperature)
+
+//? Import/Export files
